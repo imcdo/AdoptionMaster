f58c78e8343bb8611b68949029b2e7d3dec77345
imcdo <ian_mcdonald@rocketmail.com>
2019-01-26 00:31:14 -0600

    setup for stats system and people spawning beginning

---
+++ Assets/Scripts/DogGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogGenerator : MonoBehaviour
+{
+    public GameObject GenerateDog()
+    {
+        GameObject dog = new GameObject();
+        Stats stats = dog.AddComponent<Stats>();
+        return dog;
+    }
+}
---
+++ Assets/Scripts/GameStatusManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatusManager : MonoBehaviour
+{
+    public float time { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+    }
+
+    public float DetermineWaitTime()
+    {
+        if (time == 0.0f) return 10.0f;
+        else if (time > 100.0f) return 1.0f;
+        return 10.0f / (100 - time);
+    }
+}
